5be4b95cdaafbca8848c6cf581c9853666dcc8a2
gordo140427 <antonio_rivero555@hotmail.es>
2022-01-28 15:42:13 +0100

    test2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -17,7 +17,7 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-  var mayoriaDeEdad = (25 >= 18){
+  var mayoriaDeEdad = (25 >= 18)
     return allowed;
   } if (18 <= 17){
     return not allowed;
